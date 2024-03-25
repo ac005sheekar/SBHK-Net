26d9c92f47c309f3c4c071a1deb7c27ee098c6da
Sheekar Banerjee <32336082+ac005sheekar@users.noreply.github.com>
2024-03-25 21:41:28 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+# Testing the modified YOLO-v7 with Optimized Robot-vision Neural Network (SBHK-Net)
 # Written and Tested By: Sheekar Banerjee
 # Supervised By: Dr. Humayun Kabir
 # Inha University, South Korea
